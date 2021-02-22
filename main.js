5a51b6c85f96a23c4e6699a87e3602ee138ca871
Prachee-Personal <pracheesingh07@gmail.com>
2021-02-22 14:55:04 +0530

    Final commit with message on count expired.

--- main.js
+++ main.js
@@ -5,17 +5,14 @@ var startInfo = document.getElementById('startInfo');
 var resetInfo = document.getElementById('resetInfo');
 var pauseInfo = document.getElementById('pauseInfo');
 
-
-
-
 pause.disabled = true;
 var d = document.getElementById('day');
 var h = document.getElementById('hour');
 var m = document.getElementById('minute');
 var s = document.getElementById('second');
+var end = document.getElementById('end');
 
 var startTimer = null;
-
 
 function validateDays() {
     if (d.value < 0 || d.value > 365) {
@@ -23,14 +20,12 @@ function validateDays() {
     }
     return "";
 }
-
 function validateHours() {
     if (h.value < 0 || h.value > (365 * 24)) {
         return "Number of Hours can be in the range of 0 to " + (365 * 24);
     }
     return "";
 }
-
 function validateMinutes() {
     if (m.value < 0 || m.value > (365 * 24 * 60)) {
         return "Number of Minutes can be in the range of 0 to " + (365 * 24 * 60);
@@ -57,23 +52,19 @@ function calculateTotalSeconds() {
     var hoursEntered = h.value;
     var minutesEntered = m.value;
     var secondsEntered = s.value;
-    console.log("hoursEntered",hoursEntered);
-    console.log("daysEntered",daysEntered);
-    console.log("minutesEntered",minutesEntered);
-    console.log("secondsEntered",secondsEntered);
+    console.log("hoursEntered", hoursEntered);
+    console.log("daysEntered", daysEntered);
+    console.log("minutesEntered", minutesEntered);
+    console.log("secondsEntered", secondsEntered);
     var totalSecondsCalculated = Number(daysEntered * 24 * 60 * 60) + Number(hoursEntered * 60 * 60) +
         Number(minutesEntered * 60) + Number(secondsEntered);
-    //     console.log("daysCal",(daysEntered * 24 * 60 * 60))
-    //     console.log("hoursCal",(hoursEntered * 60 * 60))
-    //     console.log("minCal",(minutesEntered * 60))
-    console.log(totalSecondsCalculated);
     return totalSecondsCalculated;
 }
 
 function validateTimerValues() {
     var validateMessage = "";
     validateMessage = validateDays();
-    console.log("Mai aaya yaha")
+
     if (validateMessage.length > 0) {
         return validateMessage;
     }
@@ -97,49 +88,44 @@ function validateTimerValues() {
     validateMessage = validateMaxTimeAllowed(totalSecondsCalculated);
     if (validateMessage.length > 0) {
         return validateMessage;
-    }else {
+    } else {
         setDisplayTime(totalSecondsCalculated);
     }
     return validateMessage;
 }
 
 function setDisplayTime(totalSecondsCalculated) {
-    if (totalSecondsCalculated >= Number(24*60*60)) {
-        d.value = Math.floor(totalSecondsCalculated/Number(24 * 60 * 60));
+    if (totalSecondsCalculated >= Number(24 * 60 * 60)) {
+        d.value = Math.floor(totalSecondsCalculated / Number(24 * 60 * 60));
         totalSecondsCalculated = totalSecondsCalculated % Number(24 * 60 * 60);
-    }else {
+    } else {
         d.value = 0;
     }
 
-    if (totalSecondsCalculated >= Number(60*60)) {
-        h.value = Math.floor(totalSecondsCalculated/Number(60 * 60));
+    if (totalSecondsCalculated >= Number(60 * 60)) {
+        h.value = Math.floor(totalSecondsCalculated / Number(60 * 60));
         totalSecondsCalculated = totalSecondsCalculated % Number(60 * 60);
-    }else {
+    } else {
         h.value = 0;
     }
 
     if (totalSecondsCalculated >= Number(60)) {
-        m.value = Math.floor(totalSecondsCalculated/Number(60));
+        m.value = Math.floor(totalSecondsCalculated / Number(60));
         totalSecondsCalculated = totalSecondsCalculated % Number(60);
-    }else {
+    } else {
         m.value = 0;
     }
 
     if (totalSecondsCalculated < Number(60)) {
         s.value = totalSecondsCalculated;
     }
-    console.log("d.value",d.value);
-    console.log("h.value",h.value);
-    console.log("m.value",m.value);
-    console.log("s.value",s.value);
 }
 
 function timer() {
     if (d.value == 0 && h.value == 0 && m.value == 0 && s.value == 0) {
-        d.value = 0;
-        h.value = 0;
-        m.value = 0;
-        s.value = 0;
+            clearInterval(startTimer)
+            document.getElementById("end").innerHTML = "TIME EXPIRED";
+            pause.disabled = true;
     } else if (s.value != 0) {
         s.value--;
     } else if (m.value != 0 && s.value == 0) {
@@ -164,7 +150,6 @@ start.addEventListener('click', function () {
     if (!(d.value == 0 && h.value == 0 && m.value == 0 && s.value == 0)) {
         start.disabled = true;
         pause.disabled = false;
-        // return;
     } else {
         alert('Kindly enter a value to start the timer');
         return;
@@ -185,8 +170,6 @@ start.addEventListener('click', function () {
     }, 1000);
 })
 
-
-
 reset.addEventListener('click', function () {
     d.value = 0;
     h.value = 0;
@@ -196,8 +179,8 @@ reset.addEventListener('click', function () {
     start.disabled = false;
     pause.disabled = true;
     pause.innerHTML = "Pause";
+    document.getElementById("end").innerHTML = "";
 })
-
 
 pause.addEventListener('click', function () {
     if (pause.innerHTML == "Pause") {
@@ -211,3 +194,5 @@ pause.addEventListener('click', function () {
     }
 
 });
+
+
